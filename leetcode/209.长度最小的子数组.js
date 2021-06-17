6c8d3a4f8e56c4f75e2b54db3faa93151ee6695e
Rtdxz <1712045716@qq.com>
2021-06-17 22:57:58 +0800

    leetcode: 209长度最小子数组滑动窗口解法

--- leetcode/209.长度最小的子数组.js
+++ leetcode/209.长度最小的子数组.js
@@ -11,23 +11,43 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-  let result = Infinity;
-  let length=0
-  for (let i = 0; i < nums.length; i++){
-    sum = 0;
-    for (let j = i; j < nums.length; j++){
-      sum += nums[j];
+  // let result = Infinity;
+  // let length=0
+  // for (let i = 0; i < nums.length; i++){
+  //   sum = 0;
+  //   for (let j = i; j < nums.length; j++){
+  //     sum += nums[j];
       
-      if (sum >= target) {
-        length = j - i + 1;
-        // console.log(j,length)
-        result = result <length? result:length;
-        break;
-      }
-    }
+  //     if (sum >= target) {
+  //       length = j - i + 1;
+  //       // console.log(j,length)
+  //       result = result <length? result:length;
+  //       break;
+  //     }
+  //   }
     
+  // }
+  // return result==Infinity?0:result
+
+  let result = Infinity;
+  let length = 0;
+  let sum = 0;
+  //左指针从0开始
+  let j = 0;
+  //i为右指针
+  for (let i = 0; i < nums.length; i++){
+    sum += nums[i];
+    while (sum >= target) {
+      length = i - j + 1;
+      result = result < length ? result : length;
+      //滑动窗口
+      sum -= nums[j];
+      j++;
+     
+      
+    }
   }
-  return result==Infinity?0:result
+  return result == Infinity ? 0 : result;
 };
 // @lc code=end
 
