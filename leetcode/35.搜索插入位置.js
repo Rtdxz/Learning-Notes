fbd162b8c59d025249d9898280038a9479f64907
Rtdxz <1712045716@qq.com>
2021-06-26 12:53:04 +0800

    leetcode: 35搜索插入位置二分法

--- leetcode/35.搜索插入位置.js
+++ leetcode/35.搜索插入位置.js
@@ -10,8 +10,31 @@
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function(nums, target) {
+var searchInsert = function (nums, target) {
+  //直接做法
+  /* let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) return i;
+    if (nums[i] < target) {
+      result = i + 1
+    }
 
+  }
+  return result; */
+
+  //二分法
+  let left = 0, right = nums.length - 1;
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] < target) {
+      left = mid + 1;
+    }
+    else if (nums[mid] > target) {
+      right = mid - 1;
+    }
+    else return mid;
+  }
+  return left
 };
 // @lc code=end
 
