4a96f2116c0ab8d04301f675265556fe9b557e0f
Rtdxz <1712045716@qq.com>
2021-07-10 20:44:52 +0800

    leetcode: 242有效的字母异位词哈希做法

---
+++ leetcode/242.有效的字母异位词.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=javascript
+ *
+ * [242] 有效的字母异位词
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+  let map = new Map()
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      map.set(s[i], map.get(s[i]) + 1)
+    }
+    else {
+      map.set(s[i], 1);
+    }
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(t[i])) {
+      map.set(t[i], map.get(t[i]) - 1);
+      if (map.get(t[i]) < 0) {
+        return false
+      }
+    }
+    else {
+      return false
+    }
+  }
+  return true
+};
+// @lc code=end
+
