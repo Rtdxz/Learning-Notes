294190d9c71286b227832251f9e9c0fe497f0acd
Rtdxz <1712045716@qq.com>
2021-07-27 15:49:15 +0800

    leetcode: 347前k高频元素小顶堆做法

--- leetcode/347.前-k-个高频元素.js
+++ leetcode/347.前-k-个高频元素.js
@@ -18,57 +18,113 @@ var topKFrequent = function (nums, k) {
     items[j] = temp;
 
   }
-  let buildHeap = function (array) {
-    for (let i = Math.floor((array.length - 2) / 2); i >= 0; i--) {
-      heapify(array, array.length - 1, i)
-    }
-  }
-  let heapify = function (array, heapSize, i) {
+  //大顶堆做法
+  /*  let buildHeap = function (array) {
+     for (let i = Math.floor((array.length - 2) / 2); i >= 0; i--) {
+       heapify(array, array.length - 1, i)
+     }
+   }
+   let heapify = function (array, heapSize, i) {
+     while (true) {
+       let maxIndex = i;
+       if (i * 2 + 1 <= heapSize && array[i * 2 + 1][1] > array[maxIndex][1]) {
+         maxIndex = i * 2 + 1;
+ 
+       }
+ 
+       if (i * 2 + 2 <= heapSize && array[i * 2 + 2][1] > array[maxIndex][1]) {
+ 
+         maxIndex = i * 2 + 2;
+         console.log('r')
+       }
+       if (maxIndex == i) break;
+       swap(array, maxIndex, i);
+       i = maxIndex;
+     }
+   }
+ 
+   let map = new Map();
+   let res = []
+   for (let i = 0; i < nums.length; i++) {
+     if (map.has(nums[i])) {
+       map.set(nums[i], map.get(nums[i]) + 1)
+     }
+     else {
+       map.set(nums[i], 1);
+     }
+   }
+ 
+   if (map.size <= k) {
+     return [...map.keys()]
+   }
+   let heap = Array.from(map);
+   buildHeap(heap);
+   console.log(heap)
+   for (let i = heap.length - 1; i >= heap.length - k; i--) {
+     swap(heap, i, 0);
+     res.push(heap[i][0])
+     heapify(heap, i - 1, 0);
+     // console.log(heap)
+ 
+   }
+   return res; */
 
 
+
+  //小顶堆做法
+  let heapify = function (heap, map, k, i) {
     while (true) {
-      let maxIndex = i;
-      if (i * 2 + 1 <= heapSize && array[i * 2 + 1][1] > array[maxIndex][1]) {
-        maxIndex = i * 2 + 1;
-
+      let minIndex = i;
+      if (2 * i + 1 < k && map.get(heap[2 * i + 1]) < map.get(heap[minIndex])) {
+        minIndex = 2 * i + 1
       }
-
-      if (i * 2 + 2 <= heapSize && array[i * 2 + 2][1] > array[maxIndex][1]) {
-
-        maxIndex = i * 2 + 2;
-        console.log('r')
+      if (2 * i + 2 < k && map.get(heap[2 * i + 2]) < map.get(heap[minIndex])) {
+        minIndex = 2 * i + 2
       }
-      if (maxIndex == i) break;
-      swap(array, maxIndex, i);
-      i = maxIndex;
+      if (minIndex == i) break;
+      swap(heap, i, minIndex);
+      i = minIndex;
+    }
+
+  }
+  let buildHeap = function (array, map, k) {
+    for (let i = Math.floor((array.length - 2) / 2); i >= 0; i--) {
+      heapify(array, map, k, i)
     }
   }
-
-  let map = new Map();
-  let res = []
+  let heap = [];
+  let map = new Map()
   for (let i = 0; i < nums.length; i++) {
     if (map.has(nums[i])) {
       map.set(nums[i], map.get(nums[i]) + 1)
     }
     else {
-      map.set(nums[i], 1);
+      map.set(nums[i], 1)
     }
   }
-
   if (map.size <= k) {
     return [...map.keys()]
   }
-  let heap = Array.from(map);
-  buildHeap(heap);
-  console.log(heap)
-  for (let i = heap.length - 1; i >= heap.length - k; i--) {
-    swap(heap, i, 0);
-    res.push(heap[i][0])
-    heapify(heap, i - 1, 0);
-    // console.log(heap)
+  let array = Array.from(map);
+  console.log(array)
+  for (let i = 0; i < array.length; i++) {
+    if (i < k) {
+      heap.push(array[i][0])
+      if (heap.length == k) {
 
+        buildHeap(heap, map, k)
+
+      }
+    }
+    else if (array[i][1] > map.get(heap[0])) {
+      heap[0] = array[i][0];
+      heapify(heap, map, k, 0)
+      // console.log(heap)
+    }
   }
-  return res;
+
+  return heap.reverse();
+
 };
 // @lc code=end
 
