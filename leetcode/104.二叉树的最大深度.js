e29962f6dfbc8c555500a2f28ee3e47477b0a77e
Rtdxz <1712045716@qq.com>
2021-07-27 19:24:22 +0800

    leetcode: 104二叉树深度层次遍历做法

--- leetcode/104.二叉树的最大深度.js
+++ leetcode/104.二叉树的最大深度.js
@@ -18,8 +18,25 @@
  * @return {number}
  */
 var maxDepth = function (root) {
+  /*  if (root == null) return 0;
+   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1; */
   if (root == null) return 0;
-  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  let queue = [];
+  queue.push(root);
+  let depth = 0;
+  while (queue.length) {
+    let size = queue.length;
+    depth++;
+    for (let i = 0; i < size; i++) {
+      let top = queue.shift();
+      if (top) {
+        if (top.left) queue.push(top.left);
+        if (top.right) queue.push(top.right);
+      }
+
+    }
+  }
+  return depth
 };
 // @lc code=end
 
