9686979d6f7bc2be46741f432e729da1293f3208
Rtdxz <1712045716@qq.com>
2021-07-27 21:14:09 +0800

    leetcode: 111二叉树最小深度迭代做法

--- leetcode/111.二叉树的最小深度.js
+++ leetcode/111.二叉树的最小深度.js
@@ -18,14 +18,39 @@
  * @return {number}
  */
 var minDepth = function (root) {
-  if (root == null) return 0;
+  /* if (root == null) return 0;
   if (root.left == null && root.right != null) {
     return 1 + minDepth(root.right)
   }
   if (root.left != null && root.right == null) {
     return 1 + minDepth(root.left)
   }
-  return Math.min(minDepth(root.left), minDepth(root.right)) + 1
+  return Math.min(minDepth(root.left), minDepth(root.right)) + 1 */
+
+  let queue = [];
+  let depth = 0;
+  let flag = false
+  if (!root) return 0;
+  queue.push(root)
+  while (queue.length) {
+    let size = queue.length
+    depth++;
+    for (let i = 0; i < size; i++) {
+      let top = queue.shift();
+      if (top) {
+        if (top.left) queue.push(top.left)
+        if (top.right) queue.push(top.right)
+        if (!top.left && !top.right) {
+          flag = true
+          break;
+        }
+      }
+    }
+    if (flag) {
+      break
+    }
+  }
+  return depth
 };
 // @lc code=end
 
